b9b012533ec3fd0282bd62e707041e575c2f756e
Jordan Darlington <jordan@darlo.dev>
2022-05-03 20:49:38 +0100

    updated database connection and add home view

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ import sqlite3
 ##############################
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"
+
+def get_db_connection():
+    conn = sqlite3.connect("database/database.sqlite")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute("SELECT * FROM posts WHERE id = ?", (post_id)).fetchone()
+
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 ##############################
 # VIEWS
 ##############################
+@app.route("/")
+def home():
+    conn = get_db_connection()
+    posts = conn.execute("SELECT * FROM posts").fetchall()
+    conn.close()
+    return render_template("index.html", posts=posts)
